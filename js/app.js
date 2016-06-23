a18a64afa3e8833e84be1ae63c3bf8be856d8e7a
Daniel H <frontend@reddesignsystems.com>
2016-06-23 16:10:25 -0500

    Añadir jquery y hacer peticion ajax GET

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-
+$(function () {
+  $.ajax({
+    url: 'https://gateway.marvel.com/v1/public/characters',
+    data: {
+      ts: 1,
+      apikey: 'f1527e7aae95ea7dc033527c850532d1',
+      hash: 'c16e39a420473b8df344d530de9898e9'
+    }
+  })
+    .done(function (esponse, textStatus) {})
+    .fail(function () {
+      console.log('error')
+    })
+})
